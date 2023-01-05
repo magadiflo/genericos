3cb01998388e3004537beaf536771d2dad48f7c1
Martín <magadiflo@gmail.com>
2023-01-04 21:08:11 -0500

    Comodín <?>, para indicar que será cualquier tipo de Objeto

--- src/main/java/org/magadiflo/app/Main.java
+++ src/main/java/org/magadiflo/app/Main.java
@@ -1,5 +1,8 @@
 package org.magadiflo.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Printer<Cat> integerPrinter = new Printer<>(new Cat("Pelusa"));
@@ -12,6 +15,18 @@ public class Main {
 
         String nombre = shoutConRetorno("Gaspar", 74);
         System.out.println("Nombre retornado: " + nombre);
+
+        List<Integer> intList = new ArrayList<>();
+        intList.add(3);
+        printList(intList);
+
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Hello");
+        printList(stringList);
+
+        List<Cat> cats = new ArrayList<>();
+        cats.add(new Cat());
+        printList(cats);
     }
 
     /**
@@ -39,5 +54,20 @@ public class Main {
         System.out.println(thingToShout + "!!!!!");
         System.out.println(otherThingToShout + "!!!!!");
         return thingToShout;
+    }
+
+    /**
+     * Comodín (?)
+     * ************
+     * <?>, es usado para indicar que no sabemos exactamente cuál el tipo del genérico.
+     * <?>, es decir puede ser cualquier tipo de objeto ejmp. Cliente, String, etc..
+     * En el método se recibe una lista de <?>, es decir le decimos a java, que acepte tomar
+     * una lista de CUALQUIER COSA, pero no sé lo que va en la lista.
+     *
+     * Recordar que es lo mismo que usamos en el ResponseEntity<?>, cuando trabajamos en
+     * Spring Boot, para decirle que retornaremos cualquier tipo de Objeto: Un Map, un Cliente, etc.
+     */
+    private static void printList(List<?> myList) {
+        System.out.println();
     }
 }
