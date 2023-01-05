a3dc26fabb059b20fb27d481e5c32fd7b94eadb1
Martín <magadiflo@gmail.com>
2023-01-05 09:21:52 -0500

    Ampliando explicación sobre el comodín ?

--- src/main/java/org/magadiflo/app/Main.java
+++ src/main/java/org/magadiflo/app/Main.java
@@ -18,12 +18,12 @@ public class Main {
 
 
         /*
-        * Anteriormente esto funcionaba, pero como ahora el método printList tiene
-        * en la lista el comodín ? que extiende de Animal, solo aquellas listas
-        * cuyo dato herede de animal los aceptará, como es el caso del Cat, mientras que el
-        * Integer o String no. Esto significa que el comodín ha sido limitado a todas
-        * aquellos tipos que hereden de Animal.
-        */
+         * Anteriormente esto funcionaba, pero como ahora el método printList tiene
+         * en la lista el comodín ? que extiende de Animal, solo aquellas listas
+         * cuyo dato herede de animal los aceptará, como es el caso del Cat, mientras que el
+         * Integer o String no. Esto significa que el comodín ha sido limitado a todas
+         * aquellos tipos que hereden de Animal.
+         */
         /*
         List<Integer> intList = new ArrayList<>();
         intList.add(3);
@@ -67,12 +67,24 @@ public class Main {
     }
 
     /**
-     * Comodín (?)
-     * ************
-     * <?>, es usado para indicar que no sabemos exactamente cuál el tipo del genérico.
+     * Comodín (?) (wildcard en inglés)
+     * ********************************
+     * <?>, es usado para indicar que no sabemos exactamente cuál es el tipo del genérico.
      * <?>, es decir puede ser cualquier tipo de objeto ejmp. Cliente, String, etc..
      * En el método se recibe una lista de <?>, es decir le decimos a java, que acepte tomar
      * una lista de CUALQUIER COSA, pero no sé lo que va en la lista.
+     * <p>
+     * Ampliando la explicación y tomando a la interfaz List<E> como ejemplo, vemos que esta
+     * interfaz ha sido construida esperando un tipo de dato E, es decir un tipo de dato genérico
+     * que definirá el tipo de dato que aceptará esta lista. Si previamente sabemos qué tipo de dato
+     * aceptará la lista, lo podemos colocar, por ejemplo: List<String>, List<Cat>, List<Client>,
+     * List<Integer>, etc.. es decir, le estamos diciendo que el genérico E será del tipo
+     * que le estamos colocando. Ahora, puede suceder que de antemano no sepamos qué tipo de dato
+     * será el que tome ese genérico E, pero sí vamos a usar la interfaz List (o cualquier otra
+     * interfaz o clase que requiera que se le defina un tipo de dato a su genérico) el único detalle
+     * es que de antemano desconocemos el tipo de dato que tomará el genérico. Entonces, como solución
+     * es que se usa el comodín ?, para indicarle que el genérico que está esperando que se le
+     * defina un tipo de dato, será del cualquier tipo.
      * <p>
      * Recordar que es lo mismo que usamos en el ResponseEntity<?>, cuando trabajamos en
      * Spring Boot, para decirle que retornaremos cualquier tipo de Objeto: Un Map, un Cliente, etc.
